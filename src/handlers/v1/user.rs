4eae35d8fa92b640021d3a7cfaffd8e883e4a30d
Shotaro <s.kamada0918@gmail.com>
2022-07-18 15:56:27 +0900

    Enabled post::user to get request body

--- src/handlers/v1/user.rs
+++ src/handlers/v1/user.rs
@@ -1,5 +1,17 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+use serde::Deserialize;
 
-pub async fn post() -> Result<HttpResponse, actix_web::Error> {
-    Ok(HttpResponse::Ok().body("user::post"))
+#[derive(Debug, Deserialize)]
+pub struct UserData {
+    pub email: String,
+    pub password: String,
+}
+
+
+pub async fn post(
+    user_data: web::Json<UserData>
+) -> Result<HttpResponse, actix_web::Error> {
+    let _user_data = user_data.into_inner();
+
+    Ok(HttpResponse::Ok().json({}))
 }
